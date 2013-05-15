c7522b74ab6bee569128a9d1b069a26c4fb3ca50
Olivier Vernin <overnin@gmail.com>
2013-05-15 10:49:27 +0300

    fix smpp transport for telecel

--- transports/telecel_bf_smpp.py
+++ transports/telecel_bf_smpp.py
@@ -2,20 +2,32 @@
 from smpp.pdu_builder import DeliverSMResp
 
 from vumi.transports.smpp.clientserver.client import unpacked_pdu_opts
-from vumi.transports.smpp.clientserver.client import EsmeTransceiver
+from vumi.transports.smpp.clientserver.client import EsmeTransceiver, EsmeTransceiverFactory
 from vumi.transports.smpp import SmppTransport
+
+from vumi import log
 
 
 class TelecelBfSmppTransport(SmppTransport):
-    
+
     def make_factory(self):
         return EsmeDataTransceiverFactory(
             self.client_config,
-            self.r_server,
+            self.redis,
             self.esme_callbacks)
 
 
-class EsmeDataTransceiverFactory(EsmeTransceiver):
+class EsmeDataTransceiverFactory(EsmeTransceiverFactory):
+
+    def buildProtocol(self, addr):
+        log.msg('Connected')
+        self.esme = EsmeDataTransceiver(
+            self.config, self.redis, self.esme_callbacks)
+        self.resetDelay()
+        return self.esme
+
+
+class EsmeDataTransceiver(EsmeTransceiver):
 
     ## Receiving sms
     def handle_data_sm(self):
