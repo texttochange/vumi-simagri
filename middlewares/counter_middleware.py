a4d5d599fb5cddc8c41ba9a4f4b28511b22e31c4
tiramakan <tiramakantraore@gmail.com>
2015-04-17 11:31:17 +0000

    ajout du log du compteur

--- middlewares/counter_middleware.py
+++ middlewares/counter_middleware.py
@@ -1,6 +1,7 @@
 import redis
 
 from vumi.middleware import BaseMiddleware
+from vumi.log import log
 
 
 class CounterMiddleware(BaseMiddleware):
@@ -10,7 +11,8 @@ class CounterMiddleware(BaseMiddleware):
         self.op_name = self.config.get('op_name', 'nooperator')
 
     def handle_outbound(self, msg, endpoint):
-        self.r_server.incr("%s:outbound_msg_counter" % self.op_name)
+        compteur =self.r_server.incr("%s:outbound_msg_counter" % self.op_name)       
+        log.msg("OUTBOUND_MSG_TOTAL %s" % compteur )        
         return msg
     
     def handle_inbound(self, msg, endpoint):
